881c387b2848deb7f3e1d158cda5d714bf7f2639
illia <umbomaxia@gmail.com>
2021-08-31 14:51:36 +0300

    init: complite task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min
+        this.max
+        this.value 
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max 
     }
 
     guess() {
-
+        return this.value = Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = this.value
     }
 
     greater() {
-
+        this.min = this.value
     }
 }
 
